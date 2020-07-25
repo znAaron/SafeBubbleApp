b4db70f24082be88e8cc57986783f3f554cb670c
Zinan Guo <zinan@vt.edu>
2020-07-25 15:07:01 -0400

    seperate from main

---
+++ safe_bubble/lib/informations.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Article {
+  Article({
+    this.title,
+    this.content,
+    this.isExpanded = false,
+  });
+
+  String title;
+  String content;
+  bool isExpanded;
+}
+
+
+class ExpansionPanelPage extends StatefulWidget {
+  ExpansionPanelPage({Key key}) : super(key: key);
+
+  @override
+  _ExpansionPanelPageState createState() => _ExpansionPanelPageState();
+}
+
+class _ExpansionPanelPageState extends State<ExpansionPanelPage> {
+  var dummyArticle1 = new Article( title: "article1", content: "content1");
+  var dummyArticle2 = new Article( title: "articlew", content: "contentw");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          child: _buildPanel(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPanel() {/*
+    return ExpansionPanelList(
+      expansionCallback: (int index, bool isExpanded) {
+        setState(() {
+          _data[index].isExpanded = !isExpanded;
+        });
+      },
+      children: _data.map<ExpansionPanel>((Item item) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              title: Text(item.headerValue),
+            );
+          },
+          body: ListTile(
+              title: Text(item.expandedValue),
+              subtitle: Text('To delete this panel, tap the trash can icon'),
+              trailing: Icon(Icons.delete),
+              onTap: () {
+                setState(() {
+                  _data.removeWhere((currentItem) => item == currentItem);
+                });
+              }),
+          isExpanded: item.isExpanded,
+        );
+      }).toList(),
+    );*/
+  }
+}
